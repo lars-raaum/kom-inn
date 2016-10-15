447f44e3001ce442dda168695d57172b901f4823
Alexander Morland <alek.morland@gmail.com>
2016-10-15 16:07:37 +0200

    Formatting fixes

--- api/controllers/hosts.php
+++ api/controllers/hosts.php
@@ -9,29 +9,29 @@ $app->get('/api/host/{id}', function ($id) use ($app) {
 });
 
 $app->get('/api/hosts', function(Request $request) use ($app) {
-	$status = $_GET['status'] ?: 1;
-	$args = [(int) $status];
-	$sql = "SELECT people.*, hosts.user_id FROM people, hosts WHERE people.id = hosts.user_id AND people.status = ?";
+    $status = $_GET['status'] ?: 1;
+    $args = [(int) $status];
+    $sql = "SELECT people.*, hosts.user_id FROM people, hosts WHERE people.id = hosts.user_id AND people.status = ?";
 
-	$children = $_GET['children'] ?: null;
-	if ($children !== null && $children == 'yes') {
-		$sql .= " AND people.children <> ?";
-		$args[] = 0;
-	}
+    $children = $_GET['children'] ?: null;
+    if ($children !== null && $children == 'yes') {
+        $sql .= " AND people.children <> ?";
+        $args[] = 0;
+    }
 
-	$men = isset($_GET['men']) ? $_GET['men'] : null;
-	if ($men !== null && $men == 'yes') {
-		$sql .= ' AND people.adults_m <> ?';
-		$args[] = 0;
-	}
+    $men = isset($_GET['men']) ? $_GET['men'] : null;
+    if ($men !== null && $men == 'yes') {
+        $sql .= ' AND people.adults_m <> ?';
+        $args[] = 0;
+    }
 
-	$women = isset($_GET['women']) ? $_GET['women'] : null;
-	if ($women !== null && $women == 'yes') {
-		$sql .= ' AND people.adults_f <> ?';
-		$args[] = 0;
-	}
-	
-	error_log("SQL [ $sql ] [" . join(', ', $args) . "]");
+    $women = isset($_GET['women']) ? $_GET['women'] : null;
+    if ($women !== null && $women == 'yes') {
+        $sql .= ' AND people.adults_f <> ?';
+        $args[] = 0;
+    }
+
+    error_log("SQL [ $sql ] [" . join(', ', $args) . "]");
     $hosts = $app['db']->fetchAll($sql, $args);
     return $app->json($hosts);
 });
