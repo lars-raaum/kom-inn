ef602b12ae22356a8bf539fe277232e50219520e
Alexander Morland <alek.morland@gmail.com>
2016-10-15 15:33:26 +0200

    Guests and Hosts query conditions

--- api/public/index.php
+++ api/public/index.php
@@ -119,16 +119,62 @@ $app->get('/api/host/{id}', function ($id) use ($app) {
     return $app->json($host);
 });
 
-$app->get('/api/hosts', function() use ($app) {
-	$sql = "SELECT people.*, hosts.user_id FROM people, hosts WHERE people.id = hosts.user_id";
-    $hosts = $app['db']->fetchAll($sql);
+$app->get('/api/hosts', function(Request $request) use ($app) {
+	$status = $_GET['status'] ?: 1;
+	$args = [(int) $status];
+	$sql = "SELECT people.*, hosts.user_id FROM people, hosts WHERE people.id = hosts.user_id AND people.status = ?";
+
+	$children = $_GET['children'] ?: null;
+	if ($children !== null && $children == 'yes') {
+		$sql .= " AND people.children <> ?";
+		$args[] = 0;
+	}
+
+	$men = isset($_GET['men']) ? $_GET['men'] : null;
+	if ($men !== null && $men == 'yes') {
+		$sql .= ' AND people.adults_m <> ?';
+		$args[] = 0;
+	}
+
+	$women = isset($_GET['women']) ? $_GET['women'] : null;
+	if ($women !== null && $women == 'yes') {
+		$sql .= ' AND people.adults_f <> ?';
+		$args[] = 0;
+	}
+	
+	error_log("SQL [ $sql ] [" . join(', ', $args) . "]");
+    $hosts = $app['db']->fetchAll($sql, $args);
     return $app->json($hosts);
 });
 
 $app->get('/api/guests', function() use ($app) {
-	$sql = "SELECT people.*, guests.user_id FROM people, guests WHERE people.id = guests.user_id";
-    $guests = $app['db']->fetchAll($sql);
+	$status = $_GET['status'] ?: 1;
+	
+	$args = [(int) $status];
+	$sql = "SELECT people.*, guests.user_id FROM people, guests WHERE people.id = guests.user_id AND people.status = ?";
+
+	$children = $_GET['children'] ?: null;
+	if ($children !== null && $children == 'yes') {
+		$sql .= " AND people.children <> ?";
+		$args[] = 0;
+	}
+
+	$men = isset($_GET['men']) ? $_GET['men'] : null;
+	if ($men !== null && $men == 'yes') {
+		$sql .= ' AND people.adults_m <> ?';
+		$args[] = 0;
+	}
+
+	$women = isset($_GET['women']) ? $_GET['women'] : null;
+	if ($women !== null && $women == 'yes') {
+		$sql .= ' AND people.adults_f <> ?';
+		$args[] = 0;
+	}
+	
+	error_log("SQL [ $sql ] [" . join(', ', $args) . "]");
+    $guests = $app['db']->fetchAll($sql, $args);
     return $app->json($guests);
 });
+
 
 $app->run();
