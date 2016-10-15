766221dd1f97ae35798a2d57d079c3607f608c50
Alexander Morland <alek.morland@gmail.com>
2016-10-15 18:09:33 +0200

    Moved database config to dist file

--- resources/configuration.php
+++ resources/configuration.php
@@ -2,13 +2,7 @@
 
 $app['debug'] = true;
 
+$connection = require_once 'connections.php';
 $app->register(new Silex\Provider\DoctrineServiceProvider(), [
-    'db.options' => [
-        'driver'    => 'pdo_mysql',
-        'host'      => '127.0.0.1',
-        'dbname'    => 'kominn',
-        'user'      => 'kominn',
-        'password'  => '',
-        'charset'   => 'utf8mb4'
-	]
+    'db.options' => $connection
 ]);
