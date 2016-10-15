211e648789665d936febfb1664401896a0a2e9bf
Alexander Morland <alek.morland@gmail.com>
2016-10-15 15:42:15 +0200

    Split api endpoints into 'controller' files

---
+++ api/controllers/guests.php
@@ -0,0 +1,40 @@
+<?php
+
+$app->get('/api/guest/{id}', function ($id) use ($app) {
+
+    $sql = "SELECT people.*, guests.food_concerns FROM people, guests WHERE people.id = guests.user_id AND people.id = ?";
+    $guest = $app['db']->fetchAssoc($sql, [(int) $id]);
+
+    return $app->json($guest);
+});
+
+
+$app->get('/api/guests', function() use ($app) {
+	$status = $_GET['status'] ?: 1;
+	
+	$args = [(int) $status];
+	$sql = "SELECT people.*, guests.user_id FROM people, guests WHERE people.id = guests.user_id AND people.status = ?";
+
+	$children = $_GET['children'] ?: null;
+	if ($children !== null && $children == 'yes') {
+		$sql .= " AND people.children <> ?";
+		$args[] = 0;
+	}
+
+	$men = isset($_GET['men']) ? $_GET['men'] : null;
+	if ($men !== null && $men == 'yes') {
+		$sql .= ' AND people.adults_m <> ?';
+		$args[] = 0;
+	}
+
+	$women = isset($_GET['women']) ? $_GET['women'] : null;
+	if ($women !== null && $women == 'yes') {
+		$sql .= ' AND people.adults_f <> ?';
+		$args[] = 0;
+	}
+	
+	error_log("SQL [ $sql ] [" . join(', ', $args) . "]");
+    $guests = $app['db']->fetchAll($sql, $args);
+    return $app->json($guests);
+});
+
---
+++ api/controllers/matches.php
@@ -0,0 +1,34 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+
+$app->post('/api/match', function(Request $request) use ($app, $types) {
+	$r = $request->request;
+	$data = [
+		'guest_id' => $r->get('guest_id'),
+		'host_id'  => $r->get('host_id'),
+		'comment'  => $r->get('comment'),
+		'updated'  => new DateTime('now'),
+		'created'  => new DateTime('now')
+	];
+	$result = $app['db']->insert('matches', $data, $types);
+	if (!$result) {
+		return $app->json(['result' => false]);
+	}
+	return $app->json(['result' => true]);
+});
+
+$app->get('/api/matches', function(Request $request) use ($app) {
+	$status = 0; // matched
+	$sql = "SELECT * FROM matches WHERE status = ?";
+	$matches = $app['db']->fetchAll($sql, [(int) $status]);
+	foreach ($matches as $k => $match) {
+
+	    $sql = "SELECT people.*, hosts.user_id FROM people, hosts WHERE people.id = hosts.user_id AND people.id = ?";
+		$matches[$k]['host'] = $app['db']->fetchAssoc($sql, [(int) $match['host_id']]);
+
+	    $sql = "SELECT people.*, guests.food_concerns FROM people, guests WHERE people.id = guests.user_id AND people.id = ?";
+	    $matches[$k]['guest'] = $app['db']->fetchAssoc($sql, [(int) $match['guest_id']]);
+	}
+	return $app->json($matches);
+});
