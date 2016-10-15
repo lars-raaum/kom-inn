209884cc66b756a6d07a0601d0188d8110907d48
Alexander Morland <alek.morland@gmail.com>
2016-10-15 14:20:33 +0200

    Added phone and matches api

--- api/public/index.php
+++ api/public/index.php
@@ -15,38 +15,41 @@ $app->before(function (Request $request) {
 });
 require_once __DIR__.'/../../resources/configuration.php';
 
-$app->post('/api/register', function(Request $request) use ($app) {
+
+$dtt = \Doctrine\DBAL\Types\Type::getType('datetime');
+$types = ['updated' => $dtt, 'created' => $dtt];
+
+$app->post('/api/register', function(Request $request) use ($app, $types) {
 	$r = $request->request;
 
 	$type = $r->get('type');
 
 	$defaults = [
-		'status' => 1,
-		'updated' => new DateTime('now'),
-		'created' => new DateTime('now')
+		'status' 	=> 1,
+		'updated' 	=> new DateTime('now'),
+		'created' 	=> new DateTime('now')
 	];
 	$data = [
-		'email' => $r->get('email'),
-		'name' => $r->get('name'),
-		'gender' => $r->get('gender'),
-		'age' => $r->get('age'),
-		'children' => $r->get('children'),
-		'adults_m' => $r->get('adults_m'),
-		'adults_f' => $r->get('adults_f'),
-		'origin' => $r->get('origin'),
-		'zipcode' => $r->get('zipcode'),
-		'address' => $r->get('address'),
-		'freetext' => $r->get('freetext'),
+		'email' 	=> $r->get('email'),
+		'name' 		=> $r->get('name'),
+		'phone' 	=> $r->get('phone'),
+		'gender' 	=> $r->get('gender'),
+		'age' 		=> $r->get('age'),
+		'children' 	=> $r->get('children'),
+		'adults_m' 	=> $r->get('adults_m'),
+		'adults_f' 	=> $r->get('adults_f'),
+		'origin' 	=> $r->get('origin'),
+		'zipcode' 	=> $r->get('zipcode'),
+		'address' 	=> $r->get('address'),
+		'freetext' 	=> $r->get('freetext'),
 	] + $defaults;
 
 	// validation
 
-	$dtt = \Doctrine\DBAL\Types\Type::getType('datetime');
-	$types = ['updated' => $dtt, 'created' => $dtt];
 	$result = $app['db']->insert('people', $data , $types);
 
 	if (!$result) {
-		return $app->json(false);
+		return $app->json(['result' => false]);
 	}
 	$user_id = $app['db']->lastInsertId();
 
@@ -66,6 +69,37 @@ $app->post('/api/register', function(Request $request) use ($app) {
     $person = $app['db']->fetchAssoc($sql, [(int) $user_id]);
 
 	return $app->json($person);
+});
+
+$app->post('/api/match', function(Request $request) use ($app, $types) {
+	$r = $request->request;
+	$data = [
+		'guest_id' => $r->get('guest_id'),
+		'host_id'  => $r->get('host_id'),
+		'comment'  => $r->get('comment'),
+		'updated'  => new DateTime('now'),
+		'created'  => new DateTime('now')
+	];
+	$result = $app['db']->insert('matches', $data, $types);
+	if (!$result) {
+		return $app->json(['result' => false]);
+	}
+	return $app->json(['result' => true]);
+});
+
+$app->get('/api/matches', function(Request $request) use ($app) {
+	$status = 0; // matched
+	$sql = "SELECT * FROM matches WHERE status = ?";
+	$matches = $app['db']->fetchAll($sql, [(int) $status]);
+	foreach ($matches as $k => $match) {
+
+	    $sql = "SELECT people.*, hosts.user_id FROM people, hosts WHERE people.id = hosts.user_id AND people.id = ?";
+		$matches[$k]['host'] = $app['db']->fetchAssoc($sql, [(int) $match['host_id']]);
+
+	    $sql = "SELECT people.*, guests.food_concerns FROM people, guests WHERE people.id = guests.user_id AND people.id = ?";
+	    $matches[$k]['guest'] = $app['db']->fetchAssoc($sql, [(int) $match['guest_id']]);
+	}
+	return $app->json($matches);
 });
 
 $app->get('/api/guest/{id}', function ($id) use ($app) {
