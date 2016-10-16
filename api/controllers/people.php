c86d93f61ec06ff2d61fc2a42389bccd0622f57c
Alexander Morland <alek.morland@gmail.com>
2016-10-16 14:07:50 +0200

    Support editing all the things on people

--- api/controllers/people.php
+++ api/controllers/people.php
@@ -14,10 +14,21 @@ $app->post('/person/{id}', function($id, Request $request) use ($app, $types) {
     $r = $request->request;
     $types = ['updated' => \Doctrine\DBAL\Types\Type::getType('datetime')];
     $data  = [
-        'status'   => $r->get('status'),
-        'visits'   => $r->get('visits'),
-        'freetext' => $r->get('freetext'),
-        'updated'  => new DateTime('now')
+        'email'     => $r->get('email'),
+        'name'      => $r->get('name'),
+        'phone'     => $r->get('phone'),
+        'gender'    => $r->get('gender'),
+        'age'       => $r->get('age'),
+        'children'  => $r->get('children'),
+        'adults_m'  => $r->get('adults_m'),
+        'adults_f'  => $r->get('adults_f'),
+        'origin'    => $r->get('origin'),
+        'zipcode'   => $r->get('zipcode'),
+        'address'   => $r->get('address'),
+        'status'    => $r->get('status'),
+        'visits'    => $r->get('visits'),
+        'freetext'  => $r->get('freetext'),
+        'updated'   => new DateTime('now')
     ];
     $result = $app['db']->update('people', $data, ['id' => (int) $id], $types);
     if (!$result) {
