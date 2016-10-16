3fffd6ced980e39c9f3af6f986caba9024683205
Jimbo Freedman <james.freedman@schibsted.com>
2016-10-16 11:54:43 +0200

    Colored pins for unmatched map

--- admin/src/js/components/unmatched/index.js
+++ admin/src/js/components/unmatched/index.js
@@ -1,7 +1,112 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import cs from 'classnames';
-import Map, { GoogleApiWrapper, Marker } from 'google-maps-react'
+import Map, { GoogleApiWrapper } from 'google-maps-react'
+
+import camelize from 'camelize';
+const evtNames = ['click', 'mouseover', 'recenter'];
+
+const wrappedPromise = function() {
+    var wrappedPromise = {},
+        promise = new Promise(function (resolve, reject) {
+            wrappedPromise.resolve = resolve;
+            wrappedPromise.reject = reject;
+        });
+    wrappedPromise.then = promise.then.bind(promise);
+    wrappedPromise.catch = promise.catch.bind(promise);
+    wrappedPromise.promise = promise;
+
+    return wrappedPromise;
+}
+
+export class Marker extends React.Component {
+
+    componentDidMount() {
+        this.markerPromise = wrappedPromise();
+        this.renderMarker();
+    }
+
+    componentDidUpdate(prevProps) {
+        if ((this.props.map !== prevProps.map) ||
+            (this.props.position !== prevProps.position)) {
+            this.renderMarker();
+        }
+    }
+
+    componentWillUnmount() {
+        if (this.marker) {
+            this.marker.setMap(null);
+        }
+    }
+
+    renderMarker() {
+        let {
+            map, google, position, mapCenter, color
+        } = this.props;
+        if (!google) {
+            return null
+        }
+
+        let pos = position || mapCenter;
+        if (!(pos instanceof google.maps.LatLng)) {
+            position = new google.maps.LatLng(pos.lat, pos.lng);
+        }
+
+        console.log(this.props);
+        var pinColor = color;
+        var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+            new google.maps.Size(21, 34),
+            new google.maps.Point(0,0),
+            new google.maps.Point(10, 34));
+        var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
+            new google.maps.Size(40, 37),
+            new google.maps.Point(0, 0),
+            new google.maps.Point(12, 35));
+
+        const pref = {
+            map: map,
+            position: position,
+            icon: pinImage,
+            shadow: pinShadow
+        };
+        this.marker = new google.maps.Marker(pref);
+
+        evtNames.forEach(e => {
+            this.marker.addListener(e, this.handleEvent(e));
+    });
+
+        this.markerPromise.resolve(this.marker);
+    }
+
+    getMarker() {
+        return this.markerPromise;
+    }
+
+    handleEvent(evt) {
+        return (e) => {
+            const evtName = `on${camelize(evt)}`
+            if (this.props[evtName]) {
+                this.props[evtName](this.props, this.marker, e);
+            }
+        }
+    }
+
+    render() {
+        return null;
+    }
+}
+
+Marker.propTypes = {
+    position: PropTypes.object,
+    map: PropTypes.object,
+    color: PropTypes.string,
+}
+
+evtNames.forEach(e => Marker.propTypes[e] = PropTypes.func)
+
+Marker.defaultProps = {
+    name: 'Marker'
+}
 
 class Person extends React.Component {
     getAdults() {
@@ -95,9 +200,9 @@ class Hosts extends React.Component {
                         };
 
                         return (
-                            <Marker key={index} position={position} title={host.name} onClick={() => this.props.selectHost(host)}/>
+                            <Marker key={index} position={position} title={host.name} color='e41a1c' onClick={() => this.props.selectHost(host)}/>
                         );
-                    }).concat(<Marker key={'guest'} position={guestPosition} icon={{ path: google.maps.SymbolPath.CIRCLE, scale: 10 }} />)}
+                    }).concat(<Marker key={'guest'} position={guestPosition} color='377eb8'  icon={{ path: google.maps.SymbolPath.CIRCLE, scale: 10 }} />)}
             </Map>
         </div>
     }
