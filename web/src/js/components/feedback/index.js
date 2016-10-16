0be207a0de343c1e50af8e2cab174c4fe4fbb27b
Sindre Gulseth <sindre.gulseth@vg.no>
2016-10-16 14:25:05 +0200

    Fix translations

--- web/src/js/components/feedback/index.js
+++ web/src/js/components/feedback/index.js
@@ -74,7 +74,7 @@ export default class Feedback extends React.Component {
         return (
             <div className="main-page">
                 <h1>Kom inn</h1>
-                <p>{translate('Mennesker som snakker norsk inviterer noen som lærer seg norsk på middagsbesøk.')}</p>
+                <p>{translate('Mennesker som snakker norsk inviterer noen som lærer seg norsk på middagsbesøk')}.</p>
                 <p>{translate('Hvis du vil komme på middag, eller invitere noen på middag registrerer du deg nedenfor. Vi matcher dere basert på hvem dere er og hvor dere bor og setter dere i kontakt for å avtale tidspunkt.')}</p>
 
                 <p>{translate('Du finner mer informasjon på')} <a href="http://www.kom-inn.org">www.kom-inn.org.</a></p>
