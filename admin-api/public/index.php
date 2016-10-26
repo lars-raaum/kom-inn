e1687977334e4d14655c4a0cc44aa7551ad3f265
Alexander Morland <alek.morland@gmail.com>
2016-10-26 17:56:28 +0200

    Removed register endpoint from admin api

--- admin-api/public/index.php
+++ admin-api/public/index.php
@@ -18,7 +18,6 @@ require_once __DIR__.'/../../resources/configuration.php';
 $dtt = \Doctrine\DBAL\Types\Type::getType('datetime');
 $types = ['updated' => $dtt, 'created' => $dtt];
 
-require_once __DIR__.'/../controllers/register.php';
 require_once __DIR__.'/../controllers/matches.php';
 require_once __DIR__.'/../controllers/hosts.php';
 require_once __DIR__.'/../controllers/guests.php';
