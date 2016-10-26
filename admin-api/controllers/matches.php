1a512280a226aa6728ecdfaaa28750f719947d59
Alexander Morland <alek.morland@gmail.com>
2016-10-26 20:38:49 +0200

    Added query logging to matches endpoints

--- admin-api/controllers/matches.php
+++ admin-api/controllers/matches.php
@@ -5,17 +5,23 @@ use Symfony\Component\HttpFoundation\Request;
 
 function getMatch($id, $app) {
 
+    $args = [(int) $id];
     $sql = "SELECT * FROM matches WHERE id = ?";
-    $match = $app['db']->fetchAssoc($sql, [(int) $id]);
+    error_log("SQL [ $sql ] [" . join(', ', $args) . "] - by [{$_SERVER['PHP_AUTH_USER']}]");
+    $match = $app['db']->fetchAssoc($sql, $args);
     if (!$match) {
         return false;
     }
 
+    $args = [(int) $match['host_id']];
     $sql = "SELECT people.*, hosts.user_id FROM people, hosts WHERE people.id = hosts.user_id AND people.id = ?";
-    $match['host'] = $app['db']->fetchAssoc($sql, [(int) $match['host_id']]);
+    error_log("SQL [ $sql ] [" . join(', ', $args) . "] - by [{$_SERVER['PHP_AUTH_USER']}]");
+    $match['host'] = $app['db']->fetchAssoc($sql, $args);
 
+    $args = [(int) $match['guest_id']];
     $sql = "SELECT people.*, guests.food_concerns FROM people, guests WHERE people.id = guests.user_id AND people.id = ?";
-    $match['guest'] = $app['db']->fetchAssoc($sql, [(int) $match['guest_id']]);
+    error_log("SQL [ $sql ] [" . join(', ', $args) . "] - by [{$_SERVER['PHP_AUTH_USER']}]");
+    $match['guest'] = $app['db']->fetchAssoc($sql, $args);
 
     return $match;
 }
@@ -32,6 +38,7 @@ $app->post('/match', function(Request $request) use ($app, $types, $dtt) {
         'updated'  => $now,
         'created'  => $now
     ];
+    error_log("INSERT match Guest[{$data['guest_id']}] Host[{$data['host_id']}] by [{$_SERVER['PHP_AUTH_USER']}]");
     $result = $app['db']->insert('matches', $data, $types);
     if (!$result) {
         return $app->json(['result' => false]);
@@ -72,8 +79,10 @@ $app->get('/match/{id}', function ($id) use ($app) {
 
 $app->post('/match/{id}', function ($id, Request $request) use ($app) {
 
+    $args = [(int) $id];
     $sql = "SELECT * FROM matches WHERE id = ?";
-    $match = $app['db']->fetchAssoc($sql, [(int) $id]);
+    error_log("SQL [ $sql ] [" . join(', ', $args) . "] - by [{$_SERVER['PHP_AUTH_USER']}]");
+    $match = $app['db']->fetchAssoc($sql, $args);
     if (!$match) {
         return $app->json(null, 404);
     }
@@ -85,6 +94,8 @@ $app->post('/match/{id}', function ($id, Request $request) use ($app) {
         'comment' => $r->get('comment'),
         'updated' => new DateTime('now')
     ];
+
+    error_log("Update Match[{$id}] by [{$_SERVER['PHP_AUTH_USER']}]");
     $result = $app['db']->update('matches', $data, ['id' => (int) $id], $types);
     if (!$result) {
         error_log("Failed to update match {$id}");
@@ -100,9 +111,10 @@ $app->post('/match/{id}', function ($id, Request $request) use ($app) {
 });
 
 $app->delete('/match/{id}', function ($id, Request $request) use ($app) {
-
+    $args = [(int) $id];
     $sql = "SELECT * FROM matches WHERE id = ?";
-    $match = $app['db']->fetchAssoc($sql, [(int) $id]);
+    error_log("SQL [ $sql ] [" . join(', ', $args) . "] - by [{$_SERVER['PHP_AUTH_USER']}]");
+    $match = $app['db']->fetchAssoc($sql, $args);
     if (!$match) {
         return $app->json(null, 404);
     }
@@ -114,6 +126,7 @@ $app->delete('/match/{id}', function ($id, Request $request) use ($app) {
         'status'  => -1,
         'updated' => new DateTime('now')
     ];
+    error_log("Soft delete Match[{$id}] by [{$_SERVER['PHP_AUTH_USER']}]");
     $result = $app['db']->update('matches', $data, ['id' => (int) $id], $types);
     if (!$result) {
         error_log("Failed to update match {$id}");
@@ -123,12 +136,14 @@ $app->delete('/match/{id}', function ($id, Request $request) use ($app) {
     $host_id  = $match['host_id'];
 
     $data = ['status' => 1, 'updated' => $now->format('Y-m-d H:i:s')];
+    error_log("Set Person[{$guest_id}] to used by [{$_SERVER['PHP_AUTH_USER']}]");
     $result = $app['db']->update('people', $data, ['id' => $guest_id]);
     if (!$result) {
         error_log("Failed to updated person {$guest_id} to be used!");
         return $app->json(['result' => false]);
     }
 
+    error_log("Set Person[{$host_id}] to used by [{$_SERVER['PHP_AUTH_USER']}]");
     $result = $app['db']->update('people', $data, ['id' => $host_id]);
     if (!$result) {
         error_log("Failed to updated person {$host_id} to be used!");
@@ -149,15 +164,20 @@ $app->get('/matches', function(Request $request) use ($app) {
 
     $args = [(int) $status];
 
+    // TODO join requests
     $sql = "SELECT * FROM matches WHERE status = ?";
+    error_log("SQL [ $sql ] [" . join(', ', $args) . "] - by [{$_SERVER['PHP_AUTH_USER']}]");
     $matches = $app['db']->fetchAll($sql, $args);
+
+    $hosts_sql = "SELECT people.*, hosts.user_id FROM people, hosts WHERE people.id = hosts.user_id AND people.id = ?";
+    error_log("SQL [ $hosts_sql ] [x] foreach match - by [{$_SERVER['PHP_AUTH_USER']}]");
+
+    $guests_sql = "SELECT people.*, guests.food_concerns FROM people, guests WHERE people.id = guests.user_id AND people.id = ?";
+    error_log("SQL [ $guests_sql ] [x] foreach match - by [{$_SERVER['PHP_AUTH_USER']}]");
+
     foreach ($matches as $k => $match) {
-
-        $sql = "SELECT people.*, hosts.user_id FROM people, hosts WHERE people.id = hosts.user_id AND people.id = ?";
-        $matches[$k]['host'] = $app['db']->fetchAssoc($sql, [(int) $match['host_id']]);
-
-        $sql = "SELECT people.*, guests.food_concerns FROM people, guests WHERE people.id = guests.user_id AND people.id = ?";
-        $matches[$k]['guest'] = $app['db']->fetchAssoc($sql, [(int) $match['guest_id']]);
+        $matches[$k]['host'] = $app['db']->fetchAssoc($hosts_sql, [(int) $match['host_id']]);
+        $matches[$k]['guest'] = $app['db']->fetchAssoc($guests_sql, [(int) $match['guest_id']]);
     }
     return $app->json($matches);
 });
