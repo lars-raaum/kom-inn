3b8f585a5b0ff490f95bff6a3c8c16353f6769f8
Alexander Morland <alek.morland@gmail.com>
2016-12-14 11:33:15 +0100

    Fixed more typos in thank you

--- web/src/js/components/thank-you/index.js
+++ web/src/js/components/thank-you/index.js
@@ -18,8 +18,7 @@ export default class ThankYou extends React.Component {
                 Vi håper å få til dette i løpet av ikke så lang tid.
                 Vi tar kontakt med deg når vi har gjester i ditt området!  </p>
                 <h3>Har du andre spørsmål?</h3>
-                <p>Les mer på <a href="http://www.kom-inn.org/#hjem">kom-inn.org</a>
-                eller send en epost til <a href="mailto:kominnoslo@gmail.com.">kominnoslo@gmail.com.</a></p>
+                <p>Les mer på <a href="http://www.kom-inn.org/#hjem">kom-inn.org</a> eller send en epost til <a href="mailto:kominnoslo@gmail.com">kominnoslo@gmail.com</a>.</p>
                 <p>Ha en fortsatt fin dag!</p>
                 <p>Hilsen oss i Kom inn.</p>
             </div>
@@ -36,9 +35,7 @@ export default class ThankYou extends React.Component {
                 som mulig. Av og til tar det bare noen dager, andre ganger tar det lenger
                 tid. Vanligvis finner vi noen innen 2 uker. </p>
                 <h3>Har du andre spørsmål?</h3>
-                <p>Les mer på <a href="http://www.kom-inn.org/#hjem">kom-inn.org</a>
-                eller send en epost til
-                <a href="mailto:kominnoslo@gmail.com.">kominnoslo@gmail.com.</a></p>
+                <p>Les mer på <a href="http://www.kom-inn.org/#hjem">kom-inn.org</a> eller send en epost til <a href="mailto:kominnoslo@gmail.com">kominnoslo@gmail.com</a>.</p>
                 <p>Ha en fortsatt fin dag!</p>
                 <p>Hilsen oss i Kom inn.</p>
             </div>
@@ -59,9 +56,7 @@ export default class ThankYou extends React.Component {
                 <h1>Takk {name}</h1>
                 <p>Vi har sendt deg en bekreftelse på epost.</p>
                 <h3>Har du spørsmål?</h3>
-                <p>Les mer på <a href="http://www.kom-inn.org/#hjem">kom-inn.org</a>
-                 eller send en epost til
-                 <a href="mailto:kominnoslo@gmail.com.">kominnoslo@gmail.com.</a></p>
+                <p>Les mer på <a href="http://www.kom-inn.org/#hjem">kom-inn.org</a> eller send en epost til <a href="mailto:kominnoslo@gmail.com">kominnoslo@gmail.com</a>.</p>
                 <p>Ha en fortsatt fin dag!</p>
                 <p>Hilsen oss i Kom inn.</p>
             </div>
