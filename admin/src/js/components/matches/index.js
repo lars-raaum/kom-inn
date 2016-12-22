855fd163baba6e16fdee67aa907deb12642c858e
Alexander Morland <alek.morland@gmail.com>
2016-12-22 13:15:43 +0100

    Added missing children from matching lists

--- admin/src/js/components/matches/index.js
+++ admin/src/js/components/matches/index.js
@@ -26,7 +26,7 @@ class Person extends React.Component {
         return <div>
             <span className="title">{this.getAdults()} adults. {person.children} children.</span>
             <div className={'info'}>
-                <span className="name">{person.name}</span> <span className="info">{person.age} år. {person.adults_f} females. {person.adults_m} males.</span> <span className="origin">{person.origin}.</span> <br />
+                <span className="name">{person.name}</span> <span className="info">{person.age} år. {person.adults_f} females. {person.adults_m} males. {person.children} children.</span> <span className="origin">{person.origin}.</span> <br />
                 <span className="phone">Phone: <a href={`tel:${person.phone}`}>{person.phone}</a></span> <span className="email">Email <a href={`mailto:${person.email}`}>{person.email}</a></span> <br />
                 <span className="address">{person.address} {person.zipcode}</span> <br />
                 <span className="bringing">{person.bringing || <i>No people description</i>}</span> <br />
