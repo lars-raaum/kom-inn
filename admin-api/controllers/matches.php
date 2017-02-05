ddf0ba766b897a85383c7b353b7d9657abd0c764
Alexander Morland <alek.morland@gmail.com>
2017-02-05 15:32:03 +0100

    Fixed some validation and responses on match api

--- admin-api/controllers/matches.php
+++ admin-api/controllers/matches.php
@@ -22,6 +22,13 @@ $app->post('/match', function(Request $request) use ($app) {
     $r = $request->request;
     $guest_id = $r->get('guest_id');
     $host_id  = $r->get('host_id');
+
+    #validation
+    if (empty($guest_id) || empty($host_id)) {
+        error_log(print_r(compact('guest_id', 'host_id'), true));
+        return $app->json(null, 400, ['X-Error-Message' => 'Missing required fields guest_id and host_id']);
+    }
+
     $data = [
         'guest_id' => $guest_id,
         'host_id'  => $host_id,
@@ -29,30 +36,30 @@ $app->post('/match', function(Request $request) use ($app) {
     ];
     $id = $app['matches']->insert($data);
     if ($id === false) {
-        return $app->json(['result' => false], 500, ['X-Error-Message' => 'Failed to insert match']);
+        return $app->json(null, 500, ['X-Error-Message' => 'Failed to insert match']);
     }
 
     $result = $app['people']->setToUsed($guest_id);
     if (!$result) {
         error_log("ERROR: Failed to updated person {$guest_id} to be used!");
-        return $app->json(['result' => false], 500, ['X-Error-Message' => 'Failed to update guest']);
+        return $app->json(null, 500, ['X-Error-Message' => 'Failed to update guest']);
     }
 
     $result = $app['people']->setToUsed($host_id);
     if (!$result) {
         error_log("Failed to updated person {$host_id} to be used!");
-        return $app->json(['result' => false], 500, ['X-Error-Message' => 'Failed to update host']);
+        return $app->json(null, 500, ['X-Error-Message' => 'Failed to update host']);
     }
 
     $match = $app['matches']->get($id);
     if (!$match) {
-        return $app->json(['result' => false], 500, ['X-Error-Message' => 'Inserted match not found!']);
+        return $app->json(null, 500, ['X-Error-Message' => 'Inserted match not found!']);
     }
 
     $result = $app['sms']->sendHostInform($match);
     $result = $app['email']->sendHostInform($match);
 
-    return $app->json(['result' => true]);
+    return $app->json($match);
 });
 
 $app->post('/match/{id}', function ($id, Request $request) use ($app) {
@@ -64,10 +71,20 @@ $app->post('/match/{id}', function ($id, Request $request) use ($app) {
     }
 
     $r = $request->request;
-    $data  = [
-        'status'  => $r->get('status'),
-        'comment' => $r->get('comment')
-    ];
+
+    $status = $r->get('status');
+    $comment = $r->get('comment');
+    $data  = [];
+    if ($comment && $comment !== $match['comment']) {
+        $data['comment'] = $comment;
+    }
+    if ($status && $status !== $match['status']) {
+        $data['status'] = $status;
+    }
+    if (empty($data)) {
+        return $app->json(null, 400, ['X-Error-Message' => 'Nothing to save']);
+    }
+
     $saved = $app['matches']->update($id, $data);
     if (!$saved) {
         return $app->json(null, 500, ['X-Error-Message' => "Unable to update Match $id"]);
