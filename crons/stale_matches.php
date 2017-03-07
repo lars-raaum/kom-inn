cc1c5343361948cba263460a1550c046fda9bf7a
Alexander Morland <alek.morland@gmail.com>
2017-03-07 22:02:04 +0100

    Removed error_log override from cron now that monolog is in place

--- crons/stale_matches.php
+++ crons/stale_matches.php
@@ -3,6 +3,4 @@
 require_once 'base.php';
 require_once 'tasks/StaleMatches.php';
 
-ini_set("error_log", "/tmp/crons.log"); // Stop stdout from geting stderr when run as CLI
-
 (new crons\tasks\StaleMatches($app))->task();
