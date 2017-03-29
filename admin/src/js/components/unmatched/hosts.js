91e470c148af7e1d4d175af0437f9ff500e68de9
Sindre Gulseth <sindre.gulseth@vg.no>
2017-03-29 18:41:31 +0200

    Seperate unmatched components

---
+++ admin/src/js/components/unmatched/hosts.js
@@ -0,0 +1,83 @@
+import React, { PropTypes } from 'react';
+
+class Hosts extends React.Component {
+    renderFilters() {
+        return <div className="filters">
+            <span className="info">
+                Show hosts with:
+            </span>
+            <label>
+                <select onChange={e => this.props.setDistance(parseInt(e.target.value, 10))}>
+                    <option value="20">20 km</option>
+                    <option value="10">10 km</option>
+                    <option value="5">5 km</option>
+                    <option value="2">2 km</option>
+                </select>
+            </label>
+            <label>
+                <input type="checkbox" onChange={e => this.props.setFilter('men', e.target.checked)} /> Male
+            </label>
+            <label>
+                <input type="checkbox" onChange={e => this.props.setFilter('women', e.target.checked)} /> Female
+            </label>
+            <label>
+                <input type="checkbox" onChange={e => this.props.setFilter('children', e.target.checked)} /> Children
+            </label>
+        </div>
+    }
+    renderMap() {
+        const { guest, hosts, filters, distance } = this.props;
+
+        console.log(guest);
+
+        const guestPosition = {};
+        if (guest.loc_lat && guest.loc_long) {
+            guestPosition.lat = parseFloat(guest.loc_lat, 10);
+            guestPosition.lng = parseFloat(guest.loc_long, 10);
+        } else {
+            return null;
+        }
+
+        const query = Object.keys(filters).filter(key => filters[key]).map(key => key + '=yes').join('&');
+
+        return <div className="map-view">
+            <Map key={guest.id + '-' + query + '-' + distance} google={window.google}
+                style={{width: '100%', height: '400px'}}
+                className={'map'}
+                zoom={12}
+                initialCenter={guestPosition}>
+                    {hosts.filter(host => {
+                        return host.loc_lat && host.loc_long;
+                    }).map((host, index) => {
+                        const position = {
+                            lat: parseFloat(host.loc_lat, 10),
+                            lng: parseFloat(host.loc_long, 10)
+                        };
+
+                        return (
+                            <Marker key={index} position={position} title={host.name} color='e41a1c' onClick={() => this.props.selectHost(host)}/>
+                        );
+                    }).concat(<Marker key={'guest'} position={guestPosition} color='377eb8'  icon={{ path: google.maps.SymbolPath.CIRCLE, scale: 10 }} />)}
+            </Map>
+        </div>
+    }
+
+    render() {
+        const { guest, hosts, selectHost } = this.props;
+
+        return <div>
+            {this.renderFilters()}
+            {this.renderMap()}
+            <ul>
+                {hosts.map(host => {
+                    const selected = this.props.selectedHost && this.props.selectedHost.id === host.id;
+                    return <Person key={host.id + '-' + (selected ? 1 : 0)}
+                        person={host}
+                        selected={selected}
+                        onClick={selectHost}
+                    />
+                })}
+            </ul>
+        </div>
+    }
+}
---
+++ admin/src/js/components/unmatched/person.js
@@ -0,0 +1,45 @@
+import React, { PropTypes } from 'react';
+import cs from 'classnames';
+
+class Person extends React.Component {
+    getAdults() {
+        const { person } = this.props;
+
+        return parseInt(person.adults_f, 10) + parseInt(person.adults_m, 10);
+    }
+
+    renderDistance() {
+        if (this.props.person.distance === undefined && (this.props.person.loc_lat === null || this.props.person.loc_long === null)) {
+            return <span className="distance">
+                <svg className="octicon octicon-alert" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path d="M8.865 1.52c-.18-.31-.51-.5-.87-.5s-.69.19-.87.5L.275 13.5c-.18.31-.18.69 0 1 .19.31.52.5.87.5h13.7c.36 0 .69-.19.86-.5.17-.31.18-.69.01-1L8.865 1.52zM8.995 13h-2v-2h2v2zm0-3h-2V6h2v4z"></path></svg>
+            </span>
+        }
+
+        if (this.props.person.distance === undefined) {
+            return null;
+        }
+
+        const distance = this.props.person.distance > 1 ? Math.floor(this.props.person.distance) : this.props.person.distance.toFixed(1);
+        return <span className="distance"><b>{distance} km</b></span>;
+    }
+
+    render() {
+        const { person, selected } = this.props;
+
+        const className = cs({ selected });
+
+        return <li className={className} onClick={() => this.props.onClick(person)}>
+            <b>{this.getAdults()} adults. {person.children} children.</b>
+            <div className={'info'}>
+                <span className="name">{person.name}</span> <span className="info">{person.age} år. {person.adults_f} females. {person.adults_m} males. {person.children} children.</span> <span className="origin">{person.origin}.</span> <br />
+                <span className="bringing">{person.bringing || <i>No people description</i>}</span> <br />
+
+                <span className="phone">Phone: <a href={`tel:${person.phone}`}>{person.phone}</a></span> <span className="email">Email <a href={`mailto:${person.email}`}>{person.email}</a></span> <br />
+                <span className="address">{person.address} {person.zipcode}</span>
+            </div>
+            <div className="freetext">{person.freetext || <i>No description</i>}</div>
+            {this.renderDistance()}
+            <div className="updated">{person.waited}</div>
+        </li>
+    }
+}
