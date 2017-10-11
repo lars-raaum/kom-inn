6ce590ddd18ce969c7033ad4913f0dc8cb95e8c2
Firas <falkhouri@expedia.com>
2017-10-11 15:13:58 -0500

    split regisrant form into smaller sections

--- web/src/js/components/register/index.js
+++ web/src/js/components/register/index.js
@@ -94,6 +94,110 @@ export default class Register extends React.Component {
         return (<div className="main-page">Takk!</div>);
     }
 
+    renderAboutYouSection(translate) {
+        return (<div>
+            <h2>{translate('Hvem er du')}?</h2>
+            <div className="form-group">
+                <div className="input-field col-1-3">
+                    <label className="input-header" htmlFor="name">{translate('Hva er navnet ditt')}</label>
+                    <input type="text" placeholder="" id="name" ref={(c) => this.form.name = c} required />
+                </div>
+
+                <div className="input-field col-1-3">
+                    <label className="input-header" htmlFor="age">{translate('Alder')}</label>
+                    <input type="number" placeholder="" max="120" id="age" ref={(c) => this.form.age = c} required />
+                </div>
+
+                <div className="radio-field col-1-3">
+                    <label className="input-header">{translate('Kjønn')}</label>
+                    <label htmlFor="gender-male"><input type="radio" name="gender" id="gender-male" onChange={() => this.setState({gender: 'male' })} />  {translate('Mann')}</label>
+                    <label htmlFor="gender-female"><input type="radio" name="gender" id="gender-female" onChange={() => this.setState({gender: 'female' })} />  {translate('Kvinne')}</label>
+                </div>
+            </div>
+            <div className="form-group">
+                <div className="input-field col-1-1">
+                    <label className="input-header" htmlFor="origin">{translate('Hvor er du fra')}</label>
+                    <input type="text" placeholder="" id="origin" ref={(c) => this.form.origin = c} required />
+                </div>
+            </div>
+        </div>)
+    }
+
+    renderYourGuestsSection(translate) {
+        return (<div>
+                <h2>{translate('Hvor mange blir med på middag i tillegg til deg')}?</h2>
+                <div className="form-group">
+                    <div className="input-field col-1-3">
+                        <label className="input-header" htmlFor="adults_female">{translate('Kvinner')}</label>
+                        <input type="number" placeholder="" max="100" id="adults_female" ref={(c) => this.form.adults_female = c} required />
+                    </div>
+
+                    <div className="input-field col-1-3">
+                        <label className="input-header" htmlFor="adults_male">{translate('Menn')}</label>
+                        <input type="number" placeholder="" max="100" id="adults_male" ref={(c) => this.form.adults_male = c} required />
+                    </div>
+
+                    <div className="input-field col-1-3">
+                        <label className="input-header" htmlFor="children">{translate('Barn')} (0-18 {translate('År').toLowerCase()})</label>
+                        <input type="number" placeholder="" max="100" id="children" ref={(c) => this.form.children = c} required />
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <div className="input-field col-1-1 no-height">
+                        <label className="input-header" htmlFor="bringing">{translate('Hvem tar du med deg? Vet vi mer er sjansen for at vi finner en god match større. Eksempelvis alder på barna.')}</label>
+                        <textarea id="bringing" ref={(c) => this.form.bringing = c}></textarea>
+                    </div>
+                </div>
+            </div>);
+    }
+    renderContactInfoSection(translate) {
+        return (<div>
+                <h2>{translate('Hvordan kan vi kontakte deg')}?</h2>
+                <div className="form-group">
+                    <div className="input-field col-1-1">
+                        <label className="input-header" htmlFor="email">{translate('Hva er e-postadressen din')}?</label>
+                        <input type="email" placeholder="" id="email" ref={(c) => this.form.email = c} required />
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <div className="input-field col-1-1">
+                        <label className="input-header" htmlFor="phone">{translate('Hva er telefonnummeret ditt')}?</label>
+                        <input type="phone" placeholder="" id="phone" ref={(c) => this.form.phone = c} required />
+                    </div>
+                </div>
+
+                <h2>{translate('Hvor bor du')}?</h2>
+                <div className="form-group">
+                    <div className="input-field col-1-1">
+                        <label className="input-header" htmlFor="address">{translate('Adresse')}</label>
+                        <input type="text" placeholder="" id="address" ref={(c) => this.form.address = c} required />
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <div className="input-field col-1-1">
+                        <label className="input-header" htmlFor="zipcode">{translate('Postnummer')}</label>
+                        <input type="text" placeholder="" id="zipcode" ref={(c) => this.form.zipcode = c} required />
+                    </div>
+                </div>
+            </div>);
+    }
+
+    renderOthersSection(translate) {
+        return (<div>
+            <h2>{translate('Annet')}?</h2>
+            {this.renderFoodConcerns()}
+            <div className="form-group">
+                <div className="input-field col-1-1 no-height">
+                    <label className="input-header" htmlFor="freetext">{translate('Er det noe annet vi trenger å vite om deg/dere')}?</label>
+                    <textarea id="freetext" ref={(c) => this.form.freetext = c} ></textarea>
+                </div>
+            </div>
+        </div>);
+    }
+
     render() {
         const translate = this.context.translate;
 
@@ -150,7 +254,7 @@ export default class Register extends React.Component {
                 </div>
             )
         }
-
+        
         return (
             <div className="main-page">
                 {intro}
@@ -159,97 +263,13 @@ export default class Register extends React.Component {
 
                     {typeForm}
 
-                    <h2>{translate('Hvem er du')}?</h2>
-                    <div className="form-group">
-                        <div className="input-field col-1-3">
-                            <label className="input-header" htmlFor="name">{translate('Hva er navnet ditt')}</label>
-                            <input type="text" placeholder="" id="name" ref={(c) => this.form.name = c} required />
-                        </div>
+                    {this.renderAboutYouSection(translate)}
 
-                        <div className="input-field col-1-3">
-                            <label className="input-header" htmlFor="age">{translate('Alder')}</label>
-                            <input type="number" placeholder="" max="120" id="age" ref={(c) => this.form.age = c} required />
-                        </div>
+                    {this.renderYourGuestsSection(translate)}
 
-                        <div className="radio-field col-1-3">
-                            <label className="input-header">{translate('Kjønn')}</label>
-                            <label htmlFor="gender-male"><input type="radio" name="gender" id="gender-male" onChange={() => this.setState({gender: 'male' })} />  {translate('Mann')}</label>
-                            <label htmlFor="gender-female"><input type="radio" name="gender" id="gender-female" onChange={() => this.setState({gender: 'female' })} />  {translate('Kvinne')}</label>
-                        </div>
-                    </div>
+                    {this.renderContactInfoSection(translate)}
 
-                    <div className="form-group">
-                        <div className="input-field col-1-1">
-                            <label className="input-header" htmlFor="origin">{translate('Hvor er du fra')}</label>
-                            <input type="text" placeholder="" id="origin" ref={(c) => this.form.origin = c} required />
-                        </div>
-                    </div>
-
-                    <h2>{translate('Hvor mange blir med på middag i tillegg til deg')}?</h2>
-                    <div className="form-group">
-                        <div className="input-field col-1-3">
-                            <label className="input-header" htmlFor="adults_female">{translate('Kvinner')}</label>
-                            <input type="number" placeholder="" max="100" id="adults_female" ref={(c) => this.form.adults_female = c} required />
-                        </div>
-
-                        <div className="input-field col-1-3">
-                            <label className="input-header" htmlFor="adults_male">{translate('Menn')}</label>
-                            <input type="number" placeholder="" max="100" id="adults_male" ref={(c) => this.form.adults_male = c} required />
-                        </div>
-
-                        <div className="input-field col-1-3">
-                            <label className="input-header" htmlFor="children">{translate('Barn')} (0-18 {translate('År').toLowerCase()})</label>
-                            <input type="number" placeholder="" max="100" id="children" ref={(c) => this.form.children = c} required />
-                        </div>
-                    </div>
-
-                    <div className="form-group">
-                        <div className="input-field col-1-1 no-height">
-                            <label className="input-header" htmlFor="bringing">{translate('Hvem tar du med deg? Vet vi mer er sjansen for at vi finner en god match større. Eksempelvis alder på barna.')}</label>
-                            <textarea id="bringing" ref={(c) => this.form.bringing = c}></textarea>
-                        </div>
-                    </div>
-
-                    <h2>{translate('Hvordan kan vi kontakte deg')}?</h2>
-                    <div className="form-group">
-                        <div className="input-field col-1-1">
-                            <label className="input-header" htmlFor="email">{translate('Hva er e-postadressen din')}?</label>
-                            <input type="email" placeholder="" id="email" ref={(c) => this.form.email = c} required />
-                        </div>
-                    </div>
-
-                    <div className="form-group">
-                        <div className="input-field col-1-1">
-                            <label className="input-header" htmlFor="phone">{translate('Hva er telefonnummeret ditt')}?</label>
-                            <input type="phone" placeholder="" id="phone" ref={(c) => this.form.phone = c} required />
-                        </div>
-                    </div>
-
-                    <h2>{translate('Hvor bor du')}?</h2>
-                    <div className="form-group">
-                        <div className="input-field col-1-1">
-                            <label className="input-header" htmlFor="address">{translate('Adresse')}</label>
-                            <input type="text" placeholder="" id="address" ref={(c) => this.form.address = c} required />
-                        </div>
-                    </div>
-
-                    <div className="form-group">
-                        <div className="input-field col-1-1">
-                            <label className="input-header" htmlFor="zipcode">{translate('Postnummer')}</label>
-                            <input type="text" placeholder="" id="zipcode" ref={(c) => this.form.zipcode = c} required />
-                        </div>
-                    </div>
-
-                    <h2>{translate('Annet')}?</h2>
-
-                    {this.renderFoodConcerns()}
-
-                    <div className="form-group">
-                        <div className="input-field col-1-1 no-height">
-                            <label className="input-header" htmlFor="freetext">{translate('Er det noe annet vi trenger å vite om deg/dere')}?</label>
-                            <textarea id="freetext" ref={(c) => this.form.freetext = c} ></textarea>
-                        </div>
-                    </div>
+                    {this.renderOthersSection(translate)}
 
                     <div className="submit">
                         <button type="submit">{translate('Send inn')}</button>
