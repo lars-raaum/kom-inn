3ae36b52040b1320ceb20d6d1116ec39b015e372
Alexander Morland <alek.morland@gmail.com>
2017-11-05 15:49:55 +0100

    Delete your info, not you

--- app/mails/Purge.php
+++ app/mails/Purge.php
@@ -37,6 +37,7 @@ class Purge
         <p>Kom inn drives på fritiden av en liten gruppe frivillige og vi rekrutterer gjester gjennom å dra på skolebesøk til Voksenopplæringene i Oslo.
         Dette gjør vi noen ganger i halvåret, så det kan ta noe tid før vi finner en match. Vi finner heller ikke matcher til alle, men jo flere vi har
         som ønsker å invitere, jo lettere er det å skape gode matcher!</p>
+        <p>Hvis du ikke trykker på en link antar vi at du ikke lenger er interessert og vi vil slette dine opplysninger.</p>
         <p>mvh<br>\n
         Kom inn<br>\n
         " . '<a href="http://www.kom-inn.org/">http://www.kom-inn.org/</a>' . "<br>\n
@@ -58,6 +59,7 @@ class Purge
         <p>For en stund siden meldte du deg på Kom inn - Lær norsk rundt middagsbordet. Vi håper du har vært på én eller flere hyggelige middager!</p>\n
         <p>Hvis du vil bli delta på en ny middag med en annen familie, <a href=\"{$url}\">klikk på denne lenken!</a></p>\n
         <p>Hvis du aldri har vært på middag, men fortsatt har lyst,  <a href=\"{$url}\">klikk på denne linken.</a></p>
+        <p>Hvis du ikke trykker på en link antar vi at du ikke lenger er interessert og vi vil slette dine opplysninger.</p>
         <p>mvh<br>\n
         Kom inn<br>\n
         " . '<a href="http://www.kom-inn.org/">http://www.kom-inn.org/</a>' . "<br>\n
@@ -79,7 +81,7 @@ class Purge
         <p>For en stund siden meldte du deg på Kom inn - Lær norsk rundt middagsbordet. Takk for det!</p>\n
         <p>Vi har dessverre ikke funnet match til deg i løpet av denne tiden, men vi håper du fortsatt kunne tenke deg å invitere på middag!</p>\n
         <p>Da trenger du bare å <a href=\"{$url}\">klikke på denne lenken!</a></p>\n
-        <p>Hvis du ikke trykker på linken antar vi at du ikke lenger er interessert og vi vil slette deg.</p>\n
+        <p>Hvis du ikke trykker på linken antar vi at du ikke lenger er interessert og vi vil slette dine opplysninger.</p>\n
         <p>Kom inn drives på fritiden av en liten gruppe frivillige og vi rekrutterer gjester gjennom å dra på skolebesøk til Voksenopplæringene i Oslo.</p>\n
         <p>Hvert semester har vi flere titalls skolebesøk og matcher flere hundre middager. Vi rekrutterer hele tiden nye gjester og vil ha behov for nye verter. Jo flere verter vi har, jo større sjanse for gode matcher!</p>\n
         <p>Som sagt: Dersom du fortsatt kunne tenke deg å invitere på middag, <a href=\"{$url}\">klikk på denne linken.</a> Dersom du ikke lenger er interessert, trenger du ikke å gjøre noe.</p>\n
@@ -112,7 +114,7 @@ class Purge
         <p>For en stund siden meldte du deg på Kom inn - Lær norsk rundt middagsbordet. Takk for det!</p>\n
         <p>Vi har dessverre ikke funnet match til deg i løpet av denne tiden, men vi håper du fortsatt kunne tenke deg å delta på middag!</p>\n
         <p>Da trenger du bare å <a href=\"{$url}\">klikke på denne lenken!</a></p>\n
-        <p>Hvis du ikke trykker på linken antar vi at du ikke lenger er interessert og vi vil slette deg.</p>\n
+        <p>Hvis du ikke trykker på linken antar vi at du ikke lenger er interessert og vi vil slette dine opplysninger.</p>\n
         <p>mvh<br>\n
         Kom inn<br>\n
         " . '<a href="http://www.kom-inn.org/">http://www.kom-inn.org/</a>' . "<br>\n
